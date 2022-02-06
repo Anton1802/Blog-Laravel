885c08bba3ae0fe8507d327a44320076ba13f8f5
Anton1802 <anton.sup1802@gmail.com>
2022-02-07 00:46:39 +0300

    start

---
+++ blog/resources/views/about.blade.php
@@ -0,0 +1,36 @@
+
+@section('title')
+О нас
+@endsection
+
+@extends('index')
+@section('content')
+<div class="clearfix"></div>
+</div>
+</div>
+<div class="container">
+<div class="content">
+	<div class="about-content">
+		<!-- about -->
+	<h3>About Us</h3>
+	<img src="images/abt_pic.jpg" alt=" " />
+	<p>Having hands on experience in creating innovative designs, I do offer design
+		solutions which harness the new possibilities of web based communication,
+		internet presence and current technology trends.
+		<label>I do specialize in all aspects of website designing, theme development,
+			designing custom home pages and landing pages At vero eos et accusamus
+			et iusto odio dignissimos ducimus
+			qui blanditiis praesentium Nam libero tempore, cum soluta nobis est eligendi
+	optio cumque nihil impedit quo minus id quod maxime placeat facere
+	possimus, omnis voluptas assumenda est, omnis dolor repellendus. voluptatum...</label></p>
+	<div class="clearfix"></div>
+<p class="Nam">Nam libero tempore, cum soluta nobis est eligendi
+	optio cumque nihil impedit quo minus id quod maxime placeat facere
+	possimus, omnis voluptas assumenda est, omnis dolor repellendus.
+	Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus
+	saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.</p>
+</div>
+	<div class="clearfix"></div>
+</div>
+</div>
+@endsection
---
+++ blog/resources/views/inc/header.blade.php
@@ -0,0 +1,47 @@
+<!-- header-section-starts -->
+<div class="header">
+  <div class="container">
+    <div class="logo">
+      <a href="/"><h1>electronics</h1></a>
+    </div>
+    <div class="pages">
+      <ul>
+        <li><a class="active" href="/">Статьи</a></li>
+        <!-- <li><a href="3dprinting.html">3D Printers</a></li> -->
+        <!-- <li><a href="404.html">Tutorials</a></li> -->
+      </ul>
+    </div>
+    <div class="navigation">
+      <ul>
+        <li><a href="/about">О нас</a></li>
+        <li><a class="active" href="/contact">Контакты</a></li>
+      </ul>
+    </div>
+    <div class="clearfix"></div>
+  </div>
+</div>
+<div class="container">
+  <div class="header-bottom">
+        <div class="type">
+      <h5>Категория</h5>
+    </div>
+    <span class="menu"></span>
+    <div class="list-nav">
+      <ul>
+        <!-- <li><a href="3dprinting.html">3D Printing</a></li>|
+        <li><a href="materials.html">Materials</a></li>|
+        <li><a href="printing.html">Printing</a></li>|
+        <li><a href="filestoprint.html">Files to Print</a></li>|
+        <li><a href="/404">Videos</a></li>|
+        <li><a href="/about">About</a></li> -->
+      </ul>
+    </div>
+    <!-- script for menu -->
+      <script>
+      $( "span.menu" ).click(function() {
+        $( ".list-nav" ).slideToggle( "slow", function() {
+          // Animation complete.
+        });
+      });
+    </script>
+    <!-- script for menu -->

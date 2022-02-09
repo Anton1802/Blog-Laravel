992a74194419e4ad716cb26f2f151a89ae9a0ba1
Anton1802 <anton.sup1802@gmail.com>
2022-02-10 01:41:52 +0300

    edit register.blade

--- blog/resources/views/auth/register.blade.php
+++ blog/resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
 <div class="autho">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-12">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <form action="{{ route('register_process') }}" method="POST">
             @csrf
             @if($errors->any())

741a4a3e4b7911bb49f3617b82353ec4936bcba6
Anton1802 <anton.sup1802@gmail.com>
2022-02-13 20:00:50 +0300

    modify header

--- blog/resources/views/dashboard/header.blade.php
+++ blog/resources/views/dashboard/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
   <div class="container-fluid">
-    <img class="me-2" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Check_green_icon.svg/215px-Check_green_icon.svg.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    <img class="me-2" src="{{ asset('favicon.ico') }}" width="30" height="30" class="d-inline-block align-top" alt="">
     <a class="navbar-brand" href="{{ route('dashboard') }}">Панель управления</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -14,7 +14,7 @@
           <a class="nav-link" href="{{ route('dashboard_add') }}">Добавить статью</a>
         </li>
       </ul>
-        <div>
+        <div class="text-end">
         Привет {{ Auth::user()->name }}!
         <a class="btn btn-sm btn-primary ms-3" href="{{ route('logout') }}">Выйти</a>
         </div>

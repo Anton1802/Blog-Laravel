a85767af533e3ed8f9878aaf5613021a275a4bc1
Anton1802 <anton.sup1802@gmail.com>
2022-02-14 12:09:51 +0300

    make ajax

--- blog/public/js/slider.js
+++ blog/public/js/slider.js
@@ -1,1 +1,41 @@
 $('.sl').slick();
+
+$('.btn.rec').click(function(){
+
+  var link = $(this).attr("href");
+  var id = $(this).prop("id")
+
+  $.ajax({
+    url: link,
+    method:"GET",
+    success: function(data)
+    {
+      $(`button.btn.#${id}`).removeClass('btn-success');
+      $(`button.btn.#${id}`).removeClass('btn-danger');
+      $(`button.btn.#${id}`).addClass(data);
+      console.log(data);
+    }
+
+  })
+
+})
+
+$('.btn.public').click(function(){
+
+  var link = $(this).attr("href");
+  var id = $(this).prop("id")
+
+  $.ajax({
+    url: link,
+    method:"GET",
+    success: function(data)
+    {
+      $(`button.btn.#${id}`).removeClass('btn-success');
+      $(`button.btn.#${id}`).removeClass('btn-danger');
+      $(`button.btn.#${id}`).addClass(data);
+      console.log(data);
+    }
+
+  })
+
+})
